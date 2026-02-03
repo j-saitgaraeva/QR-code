3a04d97aa7b1d3a9eb8a8cd4089d52ffdc99507b
j-saitgaraeva <121194088+j-saitgaraeva@users.noreply.github.com>
2026-02-03 16:49:43 +0300

    Create qrMatrix.js

---
+++ js/qrMatrix.js
@@ -0,0 +1,18 @@
+export function generateMatrix(text) {
+    const qr = qrcode(0, "M");
+    qr.addData(text);
+    qr.make();
+
+    const size = qr.getModuleCount();
+    const matrix = [];
+
+    for (let r = 0; r < size; r++) {
+        const row = [];
+        for (let c = 0; c < size; c++) {
+            row.push(qr.isDark(r, c) ? 1 : 0);
+        }
+        matrix.push(row);
+    }
+
+    return matrix;
+}
