11bf8d8c5e6b0a287bfb6574751723c34691e73b
j-saitgaraeva <121194088+j-saitgaraeva@users.noreply.github.com>
2026-02-10 18:01:16 +0300

    Update qrRenderer.js
    Другая библиотека

--- js/qrRenderer.js
+++ js/qrRenderer.js
@@ -1,15 +1,5 @@
-async function loadSvg(url) {
-    const res = await fetch(url);
-    const text = await res.text();
-    const img = new Image();
-    img.src = "data:image/svg+xml;base64," + btoa(text);
-    await img.decode();
-    return img;
-}
-
-export async function renderQR(ctx, matrix) {
+export function renderQR(ctx, matrix) {
     const modules = matrix.length;
-
     const moduleSize = 5;
     const canvasSize = modules * moduleSize;
 
@@ -21,17 +11,9 @@ export async function renderQR(ctx, matrix) {
 
     ctx.fillStyle = "#000";
 
-    // Рисуем QR без 7×7 зон глазок
     for (let r = 0; r < modules; r++) {
         for (let c = 0; c < modules; c++) {
-
-            const inTopLeft = (r < 7 && c < 7);
-            const inTopRight = (r < 7 && c >= modules - 7);
-            const inBottomLeft = (r >= modules - 7 && c < 7);
-
-            if (inTopLeft || inTopRight || inBottomLeft) continue;
-
-            if (matrix[r][c] === 1) {
+            if (matrix[r][c]) {
                 ctx.fillRect(
                     c * moduleSize,
                     r * moduleSize,
@@ -41,18 +23,4 @@ export async function renderQR(ctx, matrix) {
             }
         }
     }
-
-    // Глазки
-    const eye = await loadSvg("./js/eyes/eye.svg");
-
-    const eyePx = 7 * moduleSize;
-
-    // Левый верхний
-    ctx.drawImage(eye, 0, 0, eyePx, eyePx);
-
-    // Правый верхний
-    ctx.drawImage(eye, canvasSize - eyePx, 0, eyePx, eyePx);
-
-    // Левый нижний
-    ctx.drawImage(eye, 0, canvasSize - eyePx, eyePx, eyePx);
 }
